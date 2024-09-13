57316d7f79c32a3fcf2f565b88048e7aa80e06c9
Villode <vip520moyan@gmail.com>
2024-09-13 18:25:34 +0800

    更新 Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -27,7 +27,6 @@ function Designer() {
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
       设计师
-      <img style="display: inline-block;" src="https://img.villode.us.kg/Villode.svg" alt="Villode" />
     </span>
   )
 }
