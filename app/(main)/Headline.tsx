47eef19ea0c52c9c84fd0a456a13d65100d5f492
Villode <vip520moyan@gmail.com>
2024-09-13 17:30:23 +0800

    更新 Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -26,8 +26,14 @@ function Designer() {
         <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      设计师，<img src="https://img.villode.us.kg/Villode.svg" alt="Villode" className="w-6 h-6" />
+      设计师
     </span>
+  )
+}
+
+function SVGIcon() {
+  return (
+    <img className="group relative bg-black/5 p-1 dark:bg-white/5" src="https://img.villode.us.kg/Villode.svg" alt="Villode" />
   )
 }
 
