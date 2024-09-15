665f7ad61b86759e0046ff62a951e04e8e938332
Villode <vip520moyan@gmail.com>
2024-09-15 22:55:06 +0800

    更改为friends
    更改为friends

--- app/(main)/friends/Projects.tsx
+++ app/(main)/friends/Projects.tsx
@@ -1,4 +1,4 @@
-import { ProjectCard } from '~/app/(main)/friends/ProjectCard'
+import { friendsCard } from '~/app/(main)/friends/friendsCard'
 import { getSettings } from '~/sanity/queries'
 
 export async function friends() {
@@ -9,8 +9,8 @@ export async function friends() {
       role="list"
       className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
     >
-      {friends.map((project) => (
-        <ProjectCard project={project} key={project._id} />
+      {friends.map((friends) => (
+        <friendsCard friends={friends} key={friends._id} />
       ))}
     </ul>
   )
