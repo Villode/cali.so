40d57210581c21e0eac901c9f0ce3e51f18d877f
Villode <vip520moyan@gmail.com>
2024-09-15 23:19:51 +0800

    更新 friends.tsx

--- app/(main)/friends/friends.tsx
+++ app/(main)/friends/friends.tsx
@@ -1,15 +1,15 @@
-import { FriendsCard } from '~/app/(main)/friends/friendsCard'
+import { FriendsCard } from '~/app/(main)/friends/FriendsCard'
 import { getSettings } from '~/sanity/queries'
 
 export async function friends() {
-  const friends = (await getSettings())?.friends || []
+  const friendsList = (await getSettings())?.friends || []
 
   return (
     <ul
       role="list"
       className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
     >
-      {friends.map((friends) => (
+      {friendsList.map((friend) => (
         <FriendsCard friends={friend} key={friend._id} />
       ))}
     </ul>
