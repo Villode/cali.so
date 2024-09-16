86c895d3977e841cdddc0cdc5b999eba4a719b13
Villode <vip520moyan@gmail.com>
2024-09-16 23:13:09 +0800

    更新 BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -1,6 +1,7 @@
 import { parseDateTime } from '@zolplay/utils'
 // import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react';
 
 import {
   CalendarIcon,
@@ -15,6 +16,9 @@ import { type Post } from '~/sanity/schemas/post'
 export function BlogPostCard({ post, views }: { post: Post; views: number }) {
   const { title, slug, mainImage, publishedAt, categories, readingTime } = post
 
+  // 声明 isLoaded 的状态来处理图片加载的透明度
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <Link
       href={`/blog/${slug}`}
@@ -24,20 +28,21 @@ export function BlogPostCard({ post, views }: { post: Post; views: number }) {
         {
           '--post-image-fg': mainImage.asset.dominant?.foreground,
           '--post-image-bg': mainImage.asset.dominant?.background,
-          '--post-image': `url(${mainImage.asset.url}`,
+          '--post-image': `url(${mainImage.asset.url})`,
         } as React.CSSProperties
       }
     >
       <div className="relative aspect-[240/135] w-full">
         <Img
           src={mainImage.asset.url}
-          alt=""
-          className={`rounded-t-3xl object-cover w-full h-full transition-opacity duration-700 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
-          style={isLoaded ? {} : { filter: 'blur(20px)' }} 
-          onLoad={() => setIsLoaded(true)}  
+          alt={title}
+          className="rounded-t-3xl object-cover w-full h-full transition-opacity duration-500"
+          onLoad={() => setIsLoaded(true)}
+          style={{ opacity: isLoaded ? 1 : 0 }}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
         />
       </div>
+      
       <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
         <h2 className="z-20 text-base font-bold tracking-tight text-[--post-image-fg] opacity-70 transition-opacity group-hover:opacity-100 md:text-xl">
           {title}
