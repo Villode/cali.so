244340b6e124a33e888e327ae6dd8131b8ce3281
Villode <vip520moyan@gmail.com>
2024-09-16 23:33:26 +0800

    更新 BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -1,9 +1,6 @@
-"use client"; // 标记为客户端组件
-
 import { parseDateTime } from '@zolplay/utils'
 // import Image from 'next/image'
 import Link from 'next/link'
-import { useState, useEffect } from 'react';
 
 import {
   CalendarIcon,
@@ -18,14 +15,6 @@ import { type Post } from '~/sanity/schemas/post'
 export function BlogPostCard({ post, views }: { post: Post; views: number }) {
   const { title, slug, mainImage, publishedAt, categories, readingTime } = post
 
-  // 声明 isLoaded 的状态
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  // 调试用：检查图片加载是否触发 onLoad
-  useEffect(() => {
-    console.log("Image loaded:", isLoaded);
-  }, [isLoaded]);
-
   return (
     <Link
       href={`/blog/${slug}`}
@@ -35,24 +24,22 @@ export function BlogPostCard({ post, views }: { post: Post; views: number }) {
         {
           '--post-image-fg': mainImage.asset.dominant?.foreground,
           '--post-image-bg': mainImage.asset.dominant?.background,
-          '--post-image': `url(${mainImage.asset.url})`,
+          '--post-image': `url(${mainImage.asset.url}`,
         } as React.CSSProperties
       }
     >
       <div className="relative aspect-[240/135] w-full">
         <Img
           src={mainImage.asset.url}
-          alt={title}
-          className="rounded-t-3xl object-cover w-full h-full transition-opacity duration-500"
-          onLoad={() => {
-            console.log("Image has loaded");
-            setIsLoaded(true);  // 设置图片加载状态为 true
-          }}
-          style={{ opacity: isLoaded ? 1 : 0 }} // 根据 isLoaded 状态动态设置透明度
+          alt=""
+          className="rounded-t-3xl object-cover"
+          // placeholder="blur"
+          // blurDataURL={mainImage.asset.lqip}
+          // fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
         />
       </div>
-
+      
       <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
         <h2 className="z-20 text-base font-bold tracking-tight text-[--post-image-fg] opacity-70 transition-opacity group-hover:opacity-100 md:text-xl">
           {title}
