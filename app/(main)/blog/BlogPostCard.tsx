eec9cae6f47d7067d89d780f163e4ed768967bb3
Villode <vip520moyan@gmail.com>
2024-09-16 23:17:20 +0800

    更新 BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -1,3 +1,5 @@
+"use client"; // 添加此行将组件标记为客户端组件
+
 import { parseDateTime } from '@zolplay/utils'
 // import Image from 'next/image'
 import Link from 'next/link'
@@ -42,7 +44,7 @@ export function BlogPostCard({ post, views }: { post: Post; views: number }) {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
         />
       </div>
-      
+
       <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
         <h2 className="z-20 text-base font-bold tracking-tight text-[--post-image-fg] opacity-70 transition-opacity group-hover:opacity-100 md:text-xl">
           {title}
